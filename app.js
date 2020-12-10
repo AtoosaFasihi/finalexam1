740dd8bb1515c7ff569842e7c197567b17352fd4
Atoosa <atousa.fasihi@gmail.com>
2020-12-09 17:53:03 -0800

    updating redme

---
+++ app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById('myp');
+    if (x.style.visibility === 'hidden') {
+      x.style.visibility = 'visible';
+    } else {
+      x.style.visibility = 'hidden';
+    }
+  }
